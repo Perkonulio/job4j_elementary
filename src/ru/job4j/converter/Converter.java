ea64d16aa64b197994f2efe68c58cd98c9971823
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-11-23 21:16:10 +0300

    5. Конвертер валюты [#333098]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,11 +1,12 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public float rublesToDollars(float rubles){
-        return rubles/60;
+    public float rublesToDollars(float rubles) {
+        return rubles / 60;
     }
-    public float rublesToEuros(float rubles){
-        return rubles/70;
+
+    public float rublesToEuros(float rubles) {
+        return rubles / 70;
     }
 
     public static void main(String[] args) {
