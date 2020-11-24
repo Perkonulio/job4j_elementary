3eb69053d449f2af4cd0071a75a37052de38f051
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-11-24 14:14:05 +0300

    6. Идеальный вес [#333099]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,21 +1,21 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public double manWeight(short height){
+    public double manWeight(short height) {
         return (height - 100) * 1.15;
     }
 
-    public double womanWeight(short height){
+    public double womanWeight(short height) {
         return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
         Fit fit = new Fit();
         short height = 180;
-        System.out.println("При росте в " + height + " см, " +
-                " идеальный вес для мужчины будет " + fit.manWeight(height) + " кг");
+        System.out.println("При росте в " + height + " см, "
+                + " идеальный вес для мужчины будет " + fit.manWeight(height) + " кг");
 
-        System.out.println("При росте в " + height + " см, " +
-                " идеальный вес для женщины будет " + fit.womanWeight(height) + " кг");
+        System.out.println("При росте в " + height + " см, "
+                + " идеальный вес для женщины будет " + fit.womanWeight(height) + " кг");
     }
 }
