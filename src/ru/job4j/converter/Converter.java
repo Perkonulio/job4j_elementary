04bcf4efd12cdc62984a99069bb66f11d6b89db5
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-11-24 14:38:29 +0300

    0. Что такое тестирование [#333137]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,11 +10,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int firstRubles = 215;
+        int firstRubles = 420;
+        float dollarsExpect = 7;
+        float eurosExpect = 6;
         Converter converter = new Converter();
         float resultDollars = converter.rublesToDollars(firstRubles);
         float resultEuros = converter.rublesToEuros(firstRubles);
+        boolean passedDollars = dollarsExpect == converter.rublesToDollars(firstRubles);
+        boolean passedEuros = eurosExpect == converter.rublesToEuros(firstRubles);
         System.out.println(firstRubles + " rubles are " + resultDollars + " dollars");
+        System.out.println("Test result:" + passedDollars);
         System.out.println(firstRubles + " rubles are " + resultEuros + " euros");
+        System.out.println("Test result:" + passedEuros);
     }
 }
