cf5445106ed6edee224302222838ec4dcd1a2cfd
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-01 17:28:50 +0300

    4.4. Максимум из трех чисел [#333065]

--- test/ru/job4j/condition/MultiMaxTest.java
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -10,6 +10,20 @@ public class MultiMaxTest {
     public void returnMax() {
         MultiMax multiMax = new MultiMax();
         int expected = 4;
+        assertEquals(expected, multiMax.returnMax(4, 3, 2));
+    }
+
+    @Test
+    public void returnMax1() {
+        MultiMax multiMax = new MultiMax();
+        int expected = 4;
+        assertEquals(expected, multiMax.returnMax(3, 4, 2));
+    }
+
+    @Test
+    public void returnMax2() {
+        MultiMax multiMax = new MultiMax();
+        int expected = 4;
         assertEquals(expected, multiMax.returnMax(2, 3, 4));
     }
 }
