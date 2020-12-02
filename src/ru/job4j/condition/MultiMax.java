6d7488ddb83a8ab79c69b19c2e9fd41359e64482
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-02 13:02:51 +0300

    4.4. Максимум из трех чисел [#333065]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,22 +3,21 @@ package ru.job4j.condition;
 public class MultiMax {
     public int returnMax(int firstNum, int secondNum, int thirdNum) {
         int averageMax;
-        int max;
         if (firstNum >= secondNum) {
             averageMax = firstNum;
         } else {
             averageMax = secondNum;
         }
-        if (averageMax >= thirdNum) {
-            max = averageMax;
+        if (averageMax <= thirdNum) {
+            averageMax = thirdNum;
         } else {
-            max = thirdNum;
+            return averageMax;
         }
-        return max;
+        return averageMax;
     }
 
     public static void main(String[] args) {
         MultiMax multiMax = new MultiMax();
-        System.out.println(multiMax.returnMax(23, 2, 2));
+        System.out.println(multiMax.returnMax(10, 9, 10));
     }
 }
