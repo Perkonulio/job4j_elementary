112c0e968ca5c1aee14857c3f9d9fef6d76ccc09
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-02 19:22:38 +0300

    5.2. Создать программу, вычисляющую факториал. [#333162]

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -3,11 +3,7 @@ package ru.job4j.loop;
 public class Factorial {
     public int factorio(int finishNum) {
         int fact = 1;
-        if (finishNum == 0) {
-            return fact;
-        } else if (finishNum == 1) {
-            return fact;
-        } else if (finishNum > 1) {
+        if (finishNum > 1) {
             for (int i = 1; i <= finishNum; i++) {
                 fact *= i;
             }
