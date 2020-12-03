b43c50bf18f1ca2065adb4a3e5dfb0be53f23f4c
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-03 16:01:22 +0300

    6.0.2. Заполнение массива. [#333084]

---
+++ src/ru/job4j/array/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Square {
+    public int[] squareArray(int lastNumber) {
+        int[] result = new int[lastNumber];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i * i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Square square = new Square();
+        int[] result = square.squareArray(5);
+        for (int value : result) {
+            System.out.println(value);
+        }
+    }
+}
---
+++ test/ru/job4j/array/SquareTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquareTest {
+
+    @Test
+    public void squareArray() {
+        Square square = new Square();
+        square.squareArray(5);
+    }
+}
