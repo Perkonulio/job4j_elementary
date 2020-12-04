c15a7196a8e3a18d3a3c90ab7f3cab001c790838
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-04 10:35:25 +0300

    6.0.5. Массивы в тестах. [#333086]

--- test/ru/job4j/array/SquareTest.java
+++ test/ru/job4j/array/SquareTest.java
@@ -2,14 +2,22 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SquareTest {
 
     @Test
-    public void squareArray() {
+    public void squareArray1() {
         Square square = new Square();
         square.squareArray(5);
+    }
+
+    @Test
+    public void squareArray2() {
+        Square square = new Square();
+        int[] expected = {0, 1, 4, 9, 16};
+        assertThat(square.squareArray(5), is(expected));
     }
 
 }
