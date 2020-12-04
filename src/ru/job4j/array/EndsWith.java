07663da79375f094031982f2c3c1cce448e549ed
Dmitrii Kislyackov <dima.kislyackov@yandex.ru>
2020-12-04 13:44:16 +0300

    6.4.2. Слово заканчивается на ... [#333076]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class EndsWith {
     public boolean checkEnding(char[] firstWord, char[] secondWord) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 1; i <= secondWord.length; i++) {
-            if (firstWord[firstWord.length - i] == secondWord[secondWord.length - i]) {
-                result = true;
-            } else {
+            if (firstWord[firstWord.length - i] != secondWord[secondWord.length - i]) {
                 result = false;
                 break;
             }
